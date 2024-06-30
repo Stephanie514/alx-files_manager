3f9bff0fc20aa1cffcb517ee903a1283666d6340
Stephanie <chepkiruistephanie514@gmail.com>
2024-06-30 18:28:34 +0000

    First API

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const routes = require('./routes/index');
+const { PORT = 5000 } = process.env;
+
+const app = express();
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
