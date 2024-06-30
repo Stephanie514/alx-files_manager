358960dde5b3a2ae4d448f854488844e93506368
Stephanie <chepkiruistephanie514@gmail.com>
2024-06-30 18:59:06 +0000

    Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,30 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const existingUser = await usersCollection.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const result = await usersCollection.insertOne({ email, password: hashedPassword });
+
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+}
+
+module.exports = UsersController;
